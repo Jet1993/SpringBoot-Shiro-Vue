140a33c9abe8fac223f1ad5af14ec746fe56f96d
zhaojiantao <2069864540@qq.com>
2020-04-02 10:49:23 +0800

    修改heeexy

---
+++ back/src/main/java/com/jett/example/MyApplication.java
@@ -0,0 +1,36 @@
+package com.jett.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author: jett
+ * @description: SpringBoot启动类
+ * @date: 2017/10/24 11:55
+ */
+@SpringBootApplication
+@MapperScan("com.heeexy.example.dao")
+public class MyApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(MyApplication.class);
+		/*
+		 * 定义程序启动的动画
+		 * Banner.Mode.OFF:关闭;
+		 * Banner.Mode.CONSOLE:控制台输出，默认方式;
+		 * Banner.Mode.LOG:日志输出方式;
+		 */
+		application.setBannerMode(Banner.Mode.CONSOLE);
+		application.run(args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(MyApplication.class);
+	}
+}
